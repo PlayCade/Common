303cc0ac4c855c384c7ef97c9c8f765fcaf13b24
Glenn Hevey <glenn.hevey@gmail.com>
2021-06-14 16:34:15 +1000

    Update CadeOutputManager.cs
    Added constructor

--- src/Cade.Common/Interfaces/CadeOutputManager.cs
+++ src/Cade.Common/Interfaces/CadeOutputManager.cs
@@ -1,7 +1,14 @@
+using Veldrid;
+
 namespace Cade.Common.Interfaces
 {
     public abstract class CadeOutputManager
     {
+        protected GraphicsDevice _graphicsDevice;
+        public CadeOutputManager(GraphicsDevice graphicsDevice)
+        {
+            _graphicsDevice = graphicsDevice;
+        }
         public abstract void Setup();
         public abstract void Draw();
         public abstract void Dispose();
